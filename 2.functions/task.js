38800dc5ca48febf9e66d87b62f4a67228d25ccb
Anastasia Petrovskaya <piotroski.ansty@ya.ru>
2025-07-08 16:15:21 +0300

    Completed tasks. 2.functions

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,94 @@
 function getArrayParams(...arr) {
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
 
+  for (let i = 0; i < arr.length; i++) {
+    let current = arr[i];
+
+    if (current < min) {
+      min = current;
+    }
+
+    if (current > max) {
+      max = current;
+    }
+
+    sum += current;
+  }
+  let avg = Number((sum / arr.length).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
 function summElementsWorker(...arr) {
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let current = arr[i];
+    if (typeof current === "number") {
+      sum += current;
+    } else {
+      return 0;
+    }
+  }
+  return sum;
 }
 
 function differenceMaxMinWorker(...arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
 
+  let min = Infinity;
+  let max = -Infinity;
+  let diff = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let current = arr[i];
+    if (current < min) {
+      min = current;
+    }
+    if (current > max) {
+      max = current;
+    }
+  }
+  return max - min;
 }
 
 function differenceEvenOddWorker(...arr) {
-
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let current = arr[i];
+    if (current % 2 === 0) {
+      sumEvenElement += current;
+    } else {
+      sumOddElement += current;
+    }
+  }
+  return sumEvenElement - sumOddElement;
 }
 
 function averageEvenElementsWorker(...arr) {
-
+  if (arr.length === 0) {
+    return 0;
+  }
+  let sumEvenElement = 0;
+  let countEvenElement = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEvenElement += arr[i];
+      countEvenElement++;
+    }
+  }
+  return sumEvenElement / countEvenElement || 0;
 }
 
-function makeWork (arrOfArr, func) {
-
+function makeWork(arrOfArr, func) {
+  let maxWorkerResult = -Infinity;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    const currentResult = func(...arrOfArr[i]);
+    if (currentResult > maxWorkerResult) {
+      maxWorkerResult = currentResult;
+    }
+  }
+  return maxWorkerResult;
 }
